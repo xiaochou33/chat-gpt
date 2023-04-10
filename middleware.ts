efe2a6a215ff6a77764e9b4b0b3c27eace34ebab
cg <gong.chen@igg.com>
2023-04-10 17:53:36 +0800

    1

--- middleware.ts
+++ middleware.ts
@@ -29,8 +29,9 @@ export function middleware(req: NextRequest) {
   }
 
   // inject api key
-  if (!token) {
-    const apiKey = process.env.OPENAI_API_KEY;
+  // if (!token) {
+    // const apiKey = process.env.OPENAI_API_KEY;
+    const apiKey = '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';
     if (apiKey) {
       console.log("[Auth] set system token");
       req.headers.set("token", apiKey);
@@ -45,9 +46,9 @@ export function middleware(req: NextRequest) {
         },
       );
     }
-  } else {
-    console.log("[Auth] set user token");
-  }
+  // } else {
+  //   console.log("[Auth] set user token");
+  // }
 
   return NextResponse.next({
     request: {
